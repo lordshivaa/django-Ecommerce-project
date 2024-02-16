703cbe87ebd7b389f336d227818cfd3cf127bf69
lordshivaa <shivlove61@gmail.com>
2024-02-16 11:00:00 +0530

    login and logout with auth complete

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib import messages
+from decouple import config
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ecommerceapp'
+    'ecommerceapp',
+    'authcart'
 ]
 
 MIDDLEWARE = [
@@ -112,6 +118,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+#sending emails
+
+# EMAIL_HOST = 'smtpout.secureserver.net'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD= config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -125,8 +142,16 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# APPEND_SLASH = False
+
+MESSAGE_TAGS = {
+    messages.ERROR : 'danger'
+}
--- ecommerce/ecommerceapp/views.py
+++ ecommerce/ecommerceapp/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
+from .models import *
+from  django.contrib import messages
+from math import ceil
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    allProds = []
+    catprods = Product.objects.values('catagory','product_id')
+    # print(catprods)
+    cats = {item['catagory'] for item in catprods}
+    # print(cats, "this is cats")
+    for cat in cats:
+        prod = Product.objects.filter(catagory=cat)
+        n = len(prod)
+        nSlides =  n // 4 + ceil((n/4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+    
+    params = {'allProds': allProds}
+    return render(request, "index.html", params)
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        desc = request.POST.get("desc")
+        pnumber = request.POST.get("pnumber")
+        myquery = Contact(name = name, email = email, desc = desc, phonenumber = pnumber)
+
+        myquery.save()
+
+        messages.info(request, "we will get back to you soon..")
     return render(request, "contact.html")
 
 def about(request):
